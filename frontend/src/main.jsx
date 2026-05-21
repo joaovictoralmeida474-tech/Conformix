4a4ebfea55c0c266738b280e19a4eda1d72b8540
joaovictoralmeida474-tech <joaovictoralmeida474@gmail.com>
2026-05-21 17:25:14 -0300

    "

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import './styles/logged-in-theme.css';
+import './styles/logged-in-compact.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
